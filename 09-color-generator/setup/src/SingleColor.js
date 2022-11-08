d6e675f542724b7f720b55e447ef04abd5fa9135
Hrithik Khadka <hritik.khadka@centria.fi>
2022-11-09 00:06:01 +0200

    add copy-to-clipboard functionality

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import rgbToHex from "./utils";
 
 const SingleColor = ({ rgb, weight, index, hexColor }) => {
-  // console.log(hexColor);
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(",");
   const hex = rgbToHex(...rgb);
@@ -12,9 +11,14 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
     <article
       className={`color ${index > 10 && "color-light"} `}
       style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
     >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
     </article>
   );
 };
