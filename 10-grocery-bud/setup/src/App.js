57917bdbe8f1eb2be66ff71f8e43adc288651408
Hrithik Khadka <hritik.khadka@centria.fi>
2022-11-25 22:48:48 +0545

    functionality to remove each item

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -36,6 +36,11 @@ function App() {
     setList([]);
   };
 
+  const removeItem = (id) => {
+    showAlert(true, "danger", "item removed");
+    setList(list.filter((item) => item.id !== id));
+  };
+
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
@@ -56,7 +61,7 @@ function App() {
       </form>
       {list.length > 0 && (
         <div className="grocery-container">
-          <List items={list} />
+          <List items={list} removeItem={removeItem} />
           <button className="clear-btn" onClick={clearItems}>
             clear items
           </button>
